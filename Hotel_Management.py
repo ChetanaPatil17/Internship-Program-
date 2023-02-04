43ffd192f37e96c47ffc471c008143e90931a505
Chetana Patil <97773302+ChetanaPatil17@users.noreply.github.com>
2023-02-04 10:51:06 +0530

    Add files via upload

---
+++ Hotel_Management.py
@@ -0,0 +1,69 @@
+print("  **** SHINE PARADISE **** ")
+user_name=input("plzz Enter your name: ")
+print(f"Heyy!! {user_name} Welcome to Shine Paradise..")
+a = "Wedding"
+b = "Birthday Party"
+choice=input("Enter your choice: a.wedding and b.birthday party: ")
+if choice =='a':
+    print("____ Wedding ____")
+elif choice == 'b':
+    print("_____ Birthday Party ____")
+else:
+    print("Invalid Choice!")
+
+if choice == 'a' or choice == 'b':
+    age=int(input("Enter your age: "))
+    if age<=30:
+        print("Ok! plzz go to the hall no 3 at second floor!")
+    elif age<18:
+        print("Ok! plzz go to the hall no 5 at second floor!")
+    elif age>30 and age<120:
+        print("Ok! plzz go to the hall no 4 at second floor!")
+    else:
+        print("Invalid age! Plzz enter valid age..")
+
+if choice == 'a' or choice == 'b':
+    meal=int(input("Plzz select your meal.. 1.veg or 2.non-veg : "))
+    if meal==1:
+        print("<<< Veg Menu >>>")
+        list = ['1.Indian', '2.South Indian', '3.Chinese',]
+        for i in list:
+            print("---", i ,"---")
+        mt=int(input("Enter choice..What kind of food you want.:- "))
+        if mt == 1:
+            print("<<<  List of Indian Food  >>>")
+            l1= ['Masala Dosa', 'Dal-Makhani', 'Aloo-Puri', 'Rajma', 'Paneer tikka']
+            for i in l1:
+                print("---", i ,"---")
+            veg_food1=input("____Enter the name of dish above list..you want in Indian Food____")
+            print("Ok..Your order will reach you soon..thank you!")
+        elif mt == 2:
+            print("<<<  List of South Indian Food  >>>")
+            l1= ['Masala Dosa', 'Vada-Sambar', 'Idli-Sambar', 'Uttapam']
+            for i in l1:
+                print("---", i ,"---")
+            veg_food2=input("____Enter the name of dish above list..you want in South-Indian Food____")
+            print("Ok..Your order will reach you soon..thank you!")
+        elif mt == 3:
+            print("<<<  List of Chinese Food  >>>")
+            l1= ['Noodles','Manchurian','Soyabean-Chilli', 'Paneer-Chilli', 'Soup']
+            for i in l1:
+                print("---", i ,"---")
+            veg_food3=input("Enter the name of dish above list..you want in Chinese Food..")
+            print("Ok..Your order will reach you soon..thank you!")
+       
+    elif meal==2:
+        print("<<< Non-Veg Menu >>>")
+        
+        list1 = ['1.Chiken tikka', '2.Fish fry', '3.Chiken Noodles',]
+        for i in list1:
+            print("---", i ,"---")
+        nVeg_food=int(input("Enter choice..What kind of food you want.."))
+        print("Ok..Your order will reach you soon..thank you!")
+       
+    else:
+        print("Invalid choice..")
+
+
+
+
---
+++ Intro.py
@@ -0,0 +1,5 @@
+name = input("Enter your full name: ")
+b_year = int(input("Enter your birth year: "))
+age = 2023- b_year
+
+print("My name is{name}I was born in ",{b_year}" and my age is",age,)
