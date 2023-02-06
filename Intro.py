153943426c49759be1642f4dd8b9073320f66165
Chetana Patil <97773302+ChetanaPatil17@users.noreply.github.com>
2023-02-06 18:56:10 +0530

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -2,4 +2,4 @@ name = input("Enter your full name: ")
 b_year = int(input("Enter your birth year: "))
 age = 2023- b_year
 
-print("My name is{name}I was born in ",{b_year}" and my age is",age,)
+print(f"My name is {name}. I was born in {b_year} and my age is {age}." )
